322701e60732e3454e04a232cb683935b800246b
ShefsMstff <sheffmustafaeff@gmail.com>
2024-09-21 15:46:05 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+// task1
+// function sumAll(...rest) {
+//     return rest.reduce((acc, item) => acc + item, 0);
+//   }
+//   console.log(sumAll(1, 2, 3, 4)); 
+
+
+// task2
+// const array=[1,2,3,4,5];
+// const[first,second,...rest]=array;
+
+
+// console.log('newArray',[first],[second],[rest])
+
+  
+// task3
+// const data={ name: 'John',
+//      age: 30,
+//       city: 'New York', 
+//       profession: 'Engineer' 
+//     }
+//     const{name,age,...rest}=data;
+//     const newData={...rest};
+//     console.log('newData',newData)
+
+
+// task4
+// function combineArrays(arr1, arr2) {
+//     return [...arr1, ...arr2];
+//   }
+  
+//   console.log(combineArrays([1, 2, 3], ['a', 'b'])); 
+
+// task5
+
+// const data1 = { name: 'Alice' };
+// const data2 = { age: 25 };
+
+// const newData = { ...data1, ...data2};
+
+// console.log(newData); 
